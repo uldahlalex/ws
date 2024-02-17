a63fb8913f71a875cc20d536c47dd7a333b4665d
Alex Uldahl Pedersen <41024316+uldahlalex@users.noreply.github.com>
2024-02-17 11:33:39 +0100

    Update Program.cs

--- api/Program.cs
+++ api/Program.cs
@@ -10,11 +10,11 @@ public static class Startup
 {
     public static void Main(string[] args)
     {
-        Statup(args);
-        Console.ReadLine();
+        var app = Statup(args);
+        app.Run();
     }
 
-    public static void Statup(string[] args)
+    public static WebApplication Statup(string[] args)
     {
         
         var builder = WebApplication.CreateBuilder(args);
@@ -55,7 +55,7 @@ public static class Startup
                 }
             };
         });
-        
+        return app;
 
     }
 }
